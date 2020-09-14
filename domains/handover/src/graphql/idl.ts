adca058b4f082d85c61ea8dc274d61f71ad47d05
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-14 13:32:13 +0200

    Minor fixes

--- domains/handover/src/graphql/idl.ts
+++ domains/handover/src/graphql/idl.ts
@@ -1,34 +1,34 @@
-const types = `
-type Handover {
-  id: ID
-  property1: String
-  property2: String
-}
-
-type CreateHandoverDto {
-  property1: String
-  property2: String
-}
-
-type UpdateHandoverDto {
-  property1: String
-  property2: String
-}
-
-type Query {
-  getHandover(id: ID!): Handover
-}
-
-type Mutation {
-  createHandover(dto: CreateHandoverDto!): Handover
-  deleteHandover(id: ID!): Boolean
-  updateHandover(id: ID!, dto: UpdateHandoverDto!): Handover
-}
-
-schema {
-  query:    Query
-  mutation: Mutation
-}
+const typeDefs = `
+  type Handover {
+    id: ID
+    property1: String
+    property2: String
+  }
+  
+  type CreateHandoverDto {
+    property1: String
+    property2: String
+  }
+  
+  type UpdateHandoverDto {
+    property1: String
+    property2: String
+  }
+  
+  type Query {
+    getHandover(id: ID!): Handover
+  }
+  
+  type Mutation {
+    createHandover(dto: CreateHandoverDto!): Handover
+    deleteHandover(id: ID!): Boolean
+    updateHandover(id: ID!, dto: UpdateHandoverDto!): Handover
+  }
+  
+  schema {
+    query:    Query
+    mutation: Mutation
+  }
 `;
 
-export default types;
+export default typeDefs;
