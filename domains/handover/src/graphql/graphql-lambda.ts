4d9e980697cfadcf3d4390c6020ae089e7cb503a
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-14 17:26:00 +0200

    Minor fixes

--- domains/handover/src/graphql/graphql-lambda.ts
+++ domains/handover/src/graphql/graphql-lambda.ts
@@ -6,5 +6,5 @@ import resolvers from "./resolvers";
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-// Todo: Use this line in your lambda and pass the body (graphql-query) as second argument.
+// Todo: Use this line in your lambda-function and pass the body (graphql-query) as the second argument.
 graphql(schema, {} as any).then((result) => {}).catch((reason) => {});
