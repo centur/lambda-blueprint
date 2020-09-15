1700f9f11d4b0ad3cbf98b252c63e0a4dc79a018
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-16 00:20:54 +0200

    Minor fixes

--- domains/customer/src/graphql/client.js
+++ domains/customer/src/graphql/client.js
@@ -3,14 +3,14 @@ const deleteQuery = "mutation($id: ID!) { deleteCustomer($id: id) }";
 const getQuery    = "query($id: ID!) { getCustomer(id: $id) { id property1 property2 } }";
 const updateQuery = "mutation($id: ID!, $dto: UpdateCustomerDto! { updateCustomer(id: $id, dto: $dto) { id } })";
 
-const request = async (body) => {
+const doRequest = async ({ query, variables }) => {
   const json = await fetch("/graphql", {
     method:  "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept":       "application/json",
     },
-    body: JSON.stringify(body),
+    body: JSON.stringify({ query, variables }),
   });
   return json.json();
 };
--- domains/handover/src/graphql/client.js
+++ domains/handover/src/graphql/client.js
@@ -3,14 +3,14 @@ const deleteQuery = "mutation($id: ID!) { deleteHandover($id: id) }";
 const getQuery    = "query($id: ID!) { getHandover(id: $id) { id property1 property2 } }";
 const updateQuery = "mutation($id: ID!, $dto: UpdateHandoverDto! { updateHandover(id: $id, dto: $dto) { id } })";
 
-const request = async (body) => {
+const doRequest = async ({ query, variables }) => {
   const json = await fetch("/graphql", {
     method:  "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept":       "application/json",
     },
-    body: JSON.stringify(body),
+    body: JSON.stringify({ query, variables }),
   });
   return json.json();
 };
