5e46aebcf22348d9e1c577684eb7411c004483c8
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-15 20:02:13 +0200

    Minor fixes

---
+++ domains/customer/src/graphql/client.js
@@ -0,0 +1,14 @@
+// When express is up, open browser plus developer-console, and use:
+
+fetch("/graphiql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept":       "application/json",
+  },
+  body: JSON.stringify({ query: "{ getCustomer(id: \"<id>\") { id } }" }) // Todo
+})
+  .then(json => json.json())
+  .then(body => console.log("Body: %s", body));
+
+// ...
