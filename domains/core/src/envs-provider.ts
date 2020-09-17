60781b03823e2f085e58cd461f41699fdef734e7
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-17 16:58:33 +0200

    Minor fixes

--- domains/core/src/envs-provider.ts
+++ domains/core/src/envs-provider.ts
@@ -1,3 +1,14 @@
 export const getEnvVar = <T>(key: string): T | undefined => {
-  throw new Error();
+  const  value = process.env[key] as any;
+  try {
+    return JSON.parse(value) as T; // Will produce the correct type.
+  } catch (e) {
+    return value as T;
+  }
+};
+
+export const getEnvVarOrElseThrows = <T>(key: string): T => {
+  const  value = getEnvVar(key);
+  if (!value) { throw new Error(`There is no value for key: ${key}`); }
+  return value as T;
 };
