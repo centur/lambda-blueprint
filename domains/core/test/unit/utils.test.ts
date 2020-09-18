3dddbe594fe2c3966d7b1eeb07e6144715505f74
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-18 23:50:02 +0200

    Minor fixes

--- domains/core/test/unit/utils.test.ts
+++ domains/core/test/unit/utils.test.ts
@@ -1,10 +1,10 @@
-import { deepMerge } from "../../src";
+import { deepCopy } from "../../src";
 
 describe("utils", () => {
-  it("should deeply merge a source-object into a target-object", () => {
+  it("should deeply copy a source-object into a target-object", () => {
     const target = { a: 1, b: { x: 1, y: 2 } };
     const source = { b: { y: 3, z: 1 }, c: 1 };
 
-    expect(deepMerge(target, source)).toEqual({ a: 1, b: { x: 1, y: 3, z: 1 }, c: 1 });
+    expect(deepCopy(target, source)).toEqual({ a: 1, b: { x: 1, y: 3, z: 1 }, c: 1 });
   });
 });
