38791c65274cdc5ff4feaf18f6fff16d1e1e25ff
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-19 00:54:22 +0200

    Minor fixes

--- domains/handover/src/experimental/ioredis/test.ts
+++ domains/handover/src/experimental/ioredis/test.ts
@@ -1,18 +1,13 @@
 import * as Redis from "ioredis";
-const cp = require("child_process");
 
 (async () => {
-  cp.execSync("docker run -d --name redis -p 6379:6379 redis:6.0.8");
-  const redis = new Redis();
+  const redis = new Redis(6379, "localhost");
 
   const key = "foo";
-  await redis.set(key, "1");
+  await redis.set(key, "bar");
 
-  const value = await redis.get(key);
-  console.debug(`Value of key ${key} is: ${value}`);
+  const val = await redis.get(key);
+  console.debug(`Val of key ${key} is: ${val}`);
 
   redis.disconnect();
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  cp.execSync("docker rm -f redis");
 })();
