edf8debcf90dca9377a567ce812ed618fa191b49
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-19 17:22:45 +0200

    Minor fixes

--- domains/customer/test/unit/create-lambda.test.ts
+++ domains/customer/test/unit/create-lambda.test.ts
@@ -7,23 +7,21 @@ import { v4 as uuidv4 } from "uuid";
 jest.mock("../../src/utils/service");
 
 describe("create-lambda", () => {
-  it("should create customer and return 201 when body of create-event is valid",
-    async () => {
-      const spyInstance = jest.spyOn(Service.prototype, "createCustomer")
-        .mockImplementation((): any => uuidv4());
+  it("should create customer and return 201 when body of create-event is valid", async () => {
+    const spyInstance = jest.spyOn(Service.prototype, "createCustomer")
+      .mockImplementation((): any => uuidv4());
 
-      const dto: CreateDto = { property1: "1", property2: "2" };
-      createEvent.body = JSON.stringify(dto);
-      const result = await entrypoint(createEvent as any);
+    const dto: CreateDto = { property1: "1", property2: "2" };
+    createEvent.body = JSON.stringify(dto);
+    const result = await entrypoint(createEvent as any);
 
-      expect(spyInstance).toHaveBeenCalledTimes (1);
-      expect(spyInstance).toHaveBeenCalledWith(dto);
+    expect(spyInstance).toHaveBeenCalledTimes (1);
+    expect(spyInstance).toHaveBeenCalledWith(dto);
 
-      // @ts-ignore
-      expect(result.headers.Location).toBeTruthy(); // Todos: Check presence of uuid in headers?
-      expect(result.statusCode).toBe(201);
-      // ...
-    }
-  );
+    // @ts-ignore
+    expect(result.headers.Location).toBeTruthy(); // Todos: Check presence of uuid in headers?
+    expect(result.statusCode).toBe(201);
+    // ...
+  });
   // ...
 });
