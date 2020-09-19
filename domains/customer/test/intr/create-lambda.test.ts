e2a66145ab8cb3db7dbe22e2ff55bf7bfa0a9bae
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-09-19 17:09:59 +0200

    Minor fixes

--- domains/customer/test/intr/create-lambda.test.ts
+++ domains/customer/test/intr/create-lambda.test.ts
@@ -1,6 +1,7 @@
 import { createTable, deleteTable } from "@lambda-blueprint/test";
 import createEvent from "../__fixtures__/create-event";
 import { entrypoint } from "../../src/create-lambda";
+import { CreateDto }  from "../../src/dtos/create-dto";
 import { Keys } from "../../src/utils/keys";
 
 describe("create-lambda", () => {
@@ -12,13 +13,15 @@ describe("create-lambda", () => {
   });
 
   it("should create customer and return 201 when body of create-event is valid", async () => {
-    createEvent.body = JSON.stringify({ property1: "1", property2: "2" });
+    const dto: CreateDto = { property1: "1", property2: "2" };
+    createEvent.body = JSON.stringify(dto);
 
     const result = await entrypoint(createEvent as any);
 
     // @ts-ignore
     expect(result.headers.Location).toBeTruthy(); // Todos: Check presence of uuid in headers?
     expect(result.statusCode).toBe(201);
+
     // ...
   });
 
--- domains/customer/test/unit/create-lambda.test.ts
+++ domains/customer/test/unit/create-lambda.test.ts
@@ -7,17 +7,10 @@ import { v4 as uuidv4 } from "uuid";
 jest.mock("../../src/utils/service");
 
 describe("create-lambda", () => {
-  it("should return 400 when body of create-event is invalid", async () => {
-    createEvent.body = JSON.stringify({ property1: 666, property2: 666 });
-
-    const result = await entrypoint(createEvent as any);
-    expect(result.statusCode).toBe(400);
-  });
-
   it("should create customer and return 201 when body of create-event is valid", async () => {
-    const spyInstance = jest.spyOn(Service.prototype, "createCustomer").mockImplementation(
-      (): any => uuidv4()
-    );
+    const spyInstance = jest.spyOn(Service.prototype, "createCustomer")
+      .mockImplementation((): any => uuidv4());
+
     const dto: CreateDto = { property1: "1", property2: "2" };
     createEvent.body = JSON.stringify(dto);
     const result = await entrypoint(createEvent as any);
@@ -28,6 +21,8 @@ describe("create-lambda", () => {
     // @ts-ignore
     expect(result.headers.Location).toBeTruthy(); // Todos: Check presence of uuid in headers?
     expect(result.statusCode).toBe(201);
+
+    // ...
   });
 
   // ...
